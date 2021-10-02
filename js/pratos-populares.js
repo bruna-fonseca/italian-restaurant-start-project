d599efaa13c05798e54b5ff540925f2e5d3cd27a
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-10-02 15:00:10 -0300

    estilizacao, add navbar responsivo e conteudo da pagina

--- js/pratos-populares.js
+++ js/pratos-populares.js
@@ -1,30 +1,6 @@
-const swiper = new Swiper('.swiper', {
-  effect: 'creative',
-  creativeEffect: {
-    prev: {
-      // will set `translateZ(-400px)` on previous slides
-      translate: [0, 0, -400],
-    },
-    next: {
-      // will set `translateX(100%)` on next slides
-      translate: ['100%', 0, 0],
-    },
-  },
+const burguerIcon = document.querySelector('#burger');
+const navBarMenu = document.querySelector('#nav-links');
 
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+burguerIcon.addEventListener('click', () => {
+  navBarMenu.classList.toggle('is-active');
 });
